81025f28319d467bee2d2af4019239ece559c875
John Bowdre <john@bowdre.net>
2022-03-21 15:19:39 -0500

    update build.sh for vsphere-iso

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 echo "Building Harbor OVA Appliance ..."
-rm -f output-vmware-iso/*.ova
+rm -f output-vsphere-iso/*.ova
 
 echo "Applying packer build to photon.pkr.hcl ..."
 packer build photon.pkr.hcl
